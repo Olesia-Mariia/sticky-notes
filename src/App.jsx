09698d5ef27f746ccc3f30d43649405d47f0d298
Olesia-Maria Kushka <86930501+Olesia-Mariia@users.noreply.github.com>
2024-06-18 00:02:44 +0300

    Add searchNote func

--- src/App.jsx
+++ src/App.jsx
@@ -32,6 +32,8 @@ function App() {
   ];
   const [notes, setNotes] = useState([]);
   const [listview, setListview] = useState(true);
+  const [search, setSearch] = useState('');
+  const [filternotes, setFilternotes] = useState([]);
 
   const addNote = (val) => {
     let newNotes = [...notes];
@@ -64,6 +66,12 @@ function App() {
     setNotes(newNotes);
   };
 
+  const searchNote = () => {
+    let newNotes = [...notes];
+    let filterData = newNotes.filter(x => x.text.includes(search))
+    setFilternotes(filterData);
+  };
+
   return (
     <div className="flex p-5 flex-row">
       <div className={`noteslist ${listview ? 'scale-100 w-[280px] h-full mr-2 bg-[#f1f1f1] border' : 'scale-0 w-0 h-0'}  flex-shrink-0 rounded overflow-hidden transition-all linear duration-700`}>
@@ -85,13 +93,37 @@ function App() {
         <div className="flex m-2 bg-slate-300 justify-center items-center">
           <input
             type="text"
+            value={search}
+            onChange={(e) => setSearch(e.target.value)}
             placeholder="Searh..."
             className="bg-transparent w-full p-2 focus-visible:outline-none"
           />
-          <Btn click={() => addNote()} icon={<IoSearch size={20} />} />
+          <Btn click={() => searchNote()} icon={<IoSearch size={20} />} />
         </div>
-        {notes.length > 0 &&
+        {search == '' && notes.length > 0 &&
           notes.map((x, i) => {
+            return (
+              <div className="m-2 relative cursor-pointer" onClick={() => updateView(i)}>
+                <div className={`noteview ${x.view ? 'active' : ''} flex flex-col w-full p-2`} style={{ backgroundColor: `${x.bgcolor}` }}>
+                  <div className="flex justify-end">
+                    <span className="text-xs">{x.createdon}</span>
+                  </div>
+                  <textarea
+                    value={x.text}
+                    readOnly
+                    placeholder="Take a note..."
+                    className="w-full cursor-pointer bg-transparent resize-none focus-visible:outline-none"
+                    name=""
+                    id=""
+                    cols="30"
+                    rows="2"
+                  ></textarea>
+                </div>
+              </div>
+            );
+          })}
+        {search != '' && filternotes.length > 0 &&
+          filternotes.map((x, i) => {
             return (
               <div className="m-2 relative cursor-pointer" onClick={() => updateView(i)}>
                 <div className={`noteview ${x.view ? 'active' : ''} flex flex-col w-full p-2`} style={{ backgroundColor: `${x.bgcolor}` }}>
