a011b1d059d22eb86811648402b5c2a52cbf8edd
Olesia-Maria Kushka <86930501+Olesia-Mariia@users.noreply.github.com>
2024-07-12 20:41:07 +0300

    add route to create a note

--- server/index.js
+++ server/index.js
@@ -7,6 +7,24 @@ const pool = require("./db");
 app.use(cors()); //Cross-origin resource sharing
 app.use(express.json()); // access to req.body
 
+// ROUTES
+
+// create a note
+app.post("/notes", async (req, res) => {
+  try {
+    const { text, cdate, bgcolor, view } = req.body;
+    const newNote = await pool.query(
+      "INSERT INTO notes (text, cdate, bgcolor, view) VALUES($1, $2, $3, $4) RETURNING *",
+      [text, cdate, bgcolor, view]
+    );
+    res.json(newNote.rows[0]);
+  } catch (err) {
+    console.error(err.message);
+  }
+});
+
+//
+
 app.listen(5000, () => {
   console.log("Server has started on port 5000");
 });
